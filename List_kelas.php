116f666ea4f3d5c2d6629c11841409945407acf9
CendikaK <cendika_fikara_34rpl@student.smktelkom-mlg.sch.id>
2026-01-15 14:54:32 +0700

    Add files via upload

---
+++ List_kelas.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>X RPL 7</title>
+</head>
+<body>
+    <h3>X RPL 7 </h3>
+    <ol type="1">
+        <li>Ryu</li>
+        <img src="https://upload.wikimedia.org/wikipedia/commons/4/4e/Macaca_nigra_self-portrait_large.jpg" alt="" width="200" height="100">
+        <li>Yayan</li>
+        <img src="img/Yayan.jpeg" alt="" height="100" width="200">
+        <li>Diqhal</li>
+        <img src="img/Diqhal.jpeg" alt="" height="100" width="200">
+        <li>Alfonso</li>
+        <img src="img/Alfonso.jpeg" alt="" height="100" width="200">
+        <li>Elzhar</li>
+        <img src="img/Elzhar.jpeg" alt="" height="100" width="200">
+    </ol>
+    
+</body>
+</html>
